2f3a7c21bcb4a52dca8c9a706bf4bcf8b2e7de2d
Pankaj Sharma <psharma@incurrency.com>
2014-06-23 22:42:40 +0530

    debugging pairs

--- src/com/incurrency/algorithms/pairs/Pairs.java
+++ src/com/incurrency/algorithms/pairs/Pairs.java
@@ -94,7 +94,7 @@ public class Pairs extends Strategy {
             for (PairDefinition p : targetOrders) {
             logger.log(Level.INFO,"Leve13");
                 synchronized(p.lockPosition){
-            logger.log(Level.INFO,"Leve14");
+            logger.log(Level.INFO,"Leve14, BuySymbol:{0}, Buyid:{2},ShortSymbol:{3},Shortid:{4}",new Object[]{p.buySymbol,p.buyid,p.shortSymbol,p.shortid});
                 logger.log(Level.INFO,"{0},{1},Strategy,Parameters,Symbol:{2},Minimum Size;{3},BuyRatio:{4},ShortRatio:{5},Exposure{6}",new Object[]{allAccounts,getStrategy(),Parameters.symbol.get(p.buyid).getSymbol(),Parameters.symbol.get(p.buyid).getMinsize(),p.buyratio,p.shortratio,getExposure()} );
                 logger.log(Level.INFO,"Leve15");
                int buySize = getExposure()==0?(int)(Parameters.symbol.get(p.buyid).getMinsize() * getNumberOfContracts()*p.buyratio):(int)(Parameters.symbol.get(p.buyid).getMinsize() * getExposure()/Parameters.symbol.get(p.buyid).getLastPrice()*p.buyratio);
