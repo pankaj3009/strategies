9032ecf1948f9c97bb6139c31940578161146554
Pankaj Sharma <psharma@incurrency.com>
2015-07-07 09:06:18 +0530

    changes started for swing

--- src/com/incurrency/algorithms/launch/LogWorker.java
+++ src/com/incurrency/algorithms/launch/LogWorker.java
@@ -7,6 +7,8 @@ package com.incurrency.algorithms.launch;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 import java.io.OutputStream;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  *
@@ -15,6 +17,7 @@ import java.io.OutputStream;
 public class LogWorker extends OutputStream{
     
     String message;
+    final static Lock lock = new ReentrantLock();  
     
     public LogWorker(){
        
@@ -27,10 +30,18 @@ public class LogWorker extends OutputStream{
     }
     
     public void run() {
+        try{
+        if (lock.tryLock()) {
         Object target;
         target = Launch.launch;
         EventQueue eventQueue = Toolkit.getDefaultToolkit().getSystemEventQueue();
         eventQueue.postEvent(new SimpleAWTEvent(target, message));
-
+        } 
+        }catch (Exception e){
+            
+        }finally{
+            lock.unlock();
+        }
+        
     }
 }
