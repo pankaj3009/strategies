37c80763136b62960ae651130ec8a15ce0ae5be6
Pankaj <Pankaj@Pankaj01>
2016-01-17 23:06:14 +0530

    adr Feature calculation more changes

--- src/com/incurrency/scanneradr/ADRManager.java
+++ src/com/incurrency/scanneradr/ADRManager.java
@@ -206,6 +206,8 @@ public class ADRManager implements Runnable {
                                     Parameters.symbol.get(ADRManager.compositeID).setTimeSeries(EnumBarSize.ONESECOND, currentDateTime.getTime(), new String[]{"movebackwardtime"}, new double[]{moveBackwardTime.get(index)});
                                     Parameters.symbol.get(ADRManager.compositeID).setTimeSeries(EnumBarSize.ONESECOND, currentDateTime.getTime(), new String[]{"moveforward"}, new double[]{moveForward.get(index)});
                                     Parameters.symbol.get(ADRManager.compositeID).setTimeSeries(EnumBarSize.ONESECOND, currentDateTime.getTime(), new String[]{"moveforwardtime"}, new double[]{moveForwardTime.get(index)});
+                                    mEsperEvtProcessor.sendEvent(new ADREvent(ADRTickType.T_MOVE, moveBackward.get(index)));
+                                    mEsperEvtProcessor.sendEvent(new ADREvent(ADRTickType.T_TIME, moveBackwardTime.get(index)));
                                 }
                             }
                         }
