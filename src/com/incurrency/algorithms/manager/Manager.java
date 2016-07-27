ad30aa13bb75ea60aaebb0cf4eac195684e6d7b9
psharma <psharma@notanotherfund.com>
2016-07-27 11:44:21 +0530

    bugfix

--- src/com/incurrency/algorithms/manager/Manager.java
+++ src/com/incurrency/algorithms/manager/Manager.java
@@ -55,7 +55,7 @@ public class Manager extends Strategy {
     private static final Logger logger = Logger.getLogger(Manager.class.getName());
 
     public Manager(MainAlgorithm m, Properties p, String parameterFile, ArrayList<String> accounts, Integer stratCount) {
-        super(m, "swing", "FUT", p, parameterFile, accounts, stratCount);
+        super(m, "manager", "FUT", p, parameterFile, accounts, stratCount);
         loadParameters(p);
         String[] tempStrategyArray = parameterFile.split("\\.")[0].split("-|_");
         for (BeanConnection c : Parameters.connection) {
