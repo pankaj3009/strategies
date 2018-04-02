614cea801bf0f9fa0d27bb65b0235071ee9b538f
psharma <psharma@DESKTOP-KHPHA3G>
2018-04-02 10:05:47 +0530

    check that orderfile ends with .txt dont check for delete event

--- src/com/incurrency/algorithms/manual/Manual.java
+++ src/com/incurrency/algorithms/manual/Manual.java
@@ -66,7 +66,7 @@ import redis.clients.jedis.JedisPoolConfig;
 public class Manual extends Strategy implements TradeListener {
 
     private static final Logger logger = Logger.getLogger(Manual.class.getName());
-    private static WatchService watcher=null;
+    private static WatchService watcher = null;
     String orderSource;
     Path dir;
     public boolean optionPricingUsingFutures = true;
@@ -76,7 +76,7 @@ public class Manual extends Strategy implements TradeListener {
     public String wd;
     public Boolean scaleEntry = Boolean.FALSE;
     public Boolean scaleExit = Boolean.FALSE;
-    public Boolean useCashReferenceForSLTP=Boolean.FALSE;
+    public Boolean useCashReferenceForSLTP = Boolean.FALSE;
     private final Object lockScan = new Object();
 
     public Manual(MainAlgorithm m, Properties p, String parameterFile, ArrayList<String> accounts, Boolean addTimers) {
@@ -94,7 +94,6 @@ public class Manual extends Strategy implements TradeListener {
                         watcher = dir.getFileSystem().newWatchService();
                         dir.register(watcher,
                                 ENTRY_CREATE,
-                                ENTRY_DELETE,
                                 ENTRY_MODIFY);
                     }
                     Timer tradeReader = new Timer("Timer: " + getStrategy() + " ReadTradesFromFile");
@@ -121,7 +120,7 @@ public class Manual extends Strategy implements TradeListener {
     }
 
     public void rScriptTimer() {
-        if (rServerIP != null && RScriptRunTime!=null && RStrategyFile!=null) {
+        if (rServerIP != null && RScriptRunTime != null && RStrategyFile != null) {
             Timer trigger = new Timer("Timer: " + this.getStrategy() + " RScriptProcessor");
             trigger.schedule(new RScript(), RScriptRunTime);
         }
@@ -165,42 +164,35 @@ public class Manual extends Strategy implements TradeListener {
                     }
                     if (referenceid >= 0 & (sl > 0 || tp > 0)) {
                         price = Parameters.symbol.get(referenceid).getLastPrice();
-                        sltriggered = price>0 && ((position > 0 && sl > price && sl > 0) || (position < 0 && sl < price & sl > 0)) ? Boolean.TRUE : Boolean.FALSE;
-                        tptriggered = price>0 && ((position > 0 && tp < price & tp > 0) || (position < 0 && tp > price && tp > 0)) ? Boolean.TRUE : Boolean.FALSE;
+                        sltriggered = price > 0 && ((position > 0 && sl > price && sl > 0) || (position < 0 && sl < price & sl > 0)) ? Boolean.TRUE : Boolean.FALSE;
+                        tptriggered = price > 0 && ((position > 0 && tp < price & tp > 0) || (position < 0 && tp > price && tp > 0)) ? Boolean.TRUE : Boolean.FALSE;
                     }
                 }
                 if (sltriggered | tptriggered) {
-                    if(Parameters.symbol.get(id).getBidPrice()>=Parameters.symbol.get(id).getLastPrice()*0.99 && Parameters.symbol.get(id).getAskPrice()<=Parameters.symbol.get(id).getLastPrice()*1.01 ){
-                    OrderBean ord = new OrderBean();
-                    ord.setParentDisplayName(Parameters.symbol.get(id).getDisplayname());
-                    ord.setChildDisplayName(Parameters.symbol.get(id).getDisplayname());
-                    ord.setOrderSide(position > 0 ? EnumOrderSide.SELL : EnumOrderSide.COVER);
-                    ord.setOrderReason(tptriggered ? EnumOrderReason.TP : EnumOrderReason.SL);
-                    ord.setOrderType(EnumOrderType.CUSTOMREL);
-                    ord.setOrderStage(EnumOrderStage.INIT);
-                    int size = Trade.getEntrySize(getDb(), key) - Trade.getExitSize(getDb(), key);
-                    ord.setStrategyOrderSize(size);
-                    ord.setOriginalOrderSize(size);
-                    int startingpos = Utilities.getNetPosition(Parameters.symbol, getPosition(), id, true);
-                    ord.setStrategyStartingPosition(Math.abs(startingpos));
-                    ord.setScale(scaleExit);
-                    ord.setOrderReference(getStrategy());
-                    exit(ord);
-                    }else{
-                        logger.log(Level.SEVERE,"Symbol: {0}, BidPrice: {1}, AskPrice:{2}, LastPrice:{3}, Order Not sent for execution as the prices did not meet safety check",
-                                new Object[]{Parameters.symbol.get(id).getDisplayname(),Parameters.symbol.get(id).getBidPrice(),Parameters.symbol.get(id).getAskPrice(),Parameters.symbol.get(id).getLastPrice()});
+                    if (Parameters.symbol.get(id).getBidPrice() >= Parameters.symbol.get(id).getLastPrice() * 0.99 && Parameters.symbol.get(id).getAskPrice() <= Parameters.symbol.get(id).getLastPrice() * 1.01) {
+                        OrderBean ord = new OrderBean();
+                        ord.setParentDisplayName(Parameters.symbol.get(id).getDisplayname());
+                        ord.setChildDisplayName(Parameters.symbol.get(id).getDisplayname());
+                        ord.setOrderSide(position > 0 ? EnumOrderSide.SELL : EnumOrderSide.COVER);
+                        ord.setOrderReason(tptriggered ? EnumOrderReason.TP : EnumOrderReason.SL);
+                        ord.setOrderType(EnumOrderType.CUSTOMREL);
+                        ord.setOrderStage(EnumOrderStage.INIT);
+                        int size = Trade.getEntrySize(getDb(), key) - Trade.getExitSize(getDb(), key);
+                        ord.setStrategyOrderSize(size);
+                        ord.setOriginalOrderSize(size);
+                        int startingpos = Utilities.getNetPosition(Parameters.symbol, getPosition(), id, true);
+                        ord.setStrategyStartingPosition(Math.abs(startingpos));
+                        ord.setScale(scaleExit);
+                        ord.setOrderReference(getStrategy());
+                        exit(ord);
+                    } else {
+                        logger.log(Level.SEVERE, "Symbol: {0}, BidPrice: {1}, AskPrice:{2}, LastPrice:{3}, Order Not sent for execution as the prices did not meet safety check",
+                                new Object[]{Parameters.symbol.get(id).getDisplayname(), Parameters.symbol.get(id).getBidPrice(), Parameters.symbol.get(id).getAskPrice(), Parameters.symbol.get(id).getLastPrice()});
                     }
                 }
             }
         }
     }
-
-         
-            
-
-        
-        
-    
 
     class RScript extends TimerTask {
 
@@ -256,29 +248,31 @@ public class Manual extends Strategy implements TradeListener {
                         }
                         WatchEvent<Path> ev = (WatchEvent<Path>) event;
                         Path filename = ev.context();
-                        filename = dir.resolve(filename);
-                        BufferedReader bufferedReader;
-                        if(!Files.isDirectory(filename)){
-                        try (FileReader fileReader = new FileReader(filename.getFileName().toString())) {
-                            String line = null;
-                            bufferedReader = new BufferedReader(fileReader);
-                            while ((line = bufferedReader.readLine()) != null) {
-                                OrderBean order = null;
-                                String[] contributors = line.split(":", 3);
-                                if (contributors.length == 3) {
-                                    try (Jedis jedis = jPool.getResource()) {
-                                        jedis.lpush(contributors[0] + ":" + contributors[1], contributors[2]);
+                        if (filename.toString().endsWith(".txt")) {
+                            filename = dir.resolve(filename);
+                            BufferedReader bufferedReader;
+                            if (!Files.isDirectory(filename)) {
+                                try (FileReader fileReader = new FileReader(filename.getFileName().toString())) {
+                                    String line = null;
+                                    bufferedReader = new BufferedReader(fileReader);
+                                    while ((line = bufferedReader.readLine()) != null) {
+                                        OrderBean order = null;
+                                        String[] contributors = line.split(":", 3);
+                                        if (contributors.length == 3) {
+                                            try (Jedis jedis = jPool.getResource()) {
+                                                jedis.lpush(contributors[0] + ":" + contributors[1], contributors[2]);
+                                            }
+                                        }
                                     }
+                                    bufferedReader.close();
+                                } catch (Exception e) {
+                                    key.reset();
+                                    logger.log(Level.SEVERE, null, e);
                                 }
                             }
-                            bufferedReader.close();
-                        } catch (Exception e) {
-                            key.reset();
-                            logger.log(Level.SEVERE, null, e);
-                        }
                         }
                     }
-                        key.reset();
+                    key.reset();
                 }
             } catch (InterruptedException ex) {
                 Logger.getLogger(Manual.class.getName()).log(Level.SEVERE, null, ex);
@@ -315,7 +309,7 @@ public class Manual extends Strategy implements TradeListener {
                 if (trade != null && trade.get(1) != null) {
                     OrderBean order = null;
                     if (isJSONValid(trade.get(1))) {
-                        order=jsonOrderProcessor(trade.get(1));
+                        order = jsonOrderProcessor(trade.get(1));
                     } else {
                         order = simpleOrderProcessor(trade.get(1));
                     }
@@ -369,7 +363,7 @@ public class Manual extends Strategy implements TradeListener {
                     HashMap<String, Object> tmpOrderAttributes = new HashMap<>();
                     referenceid = getUnderlyingReferenceID(symbolid);
                     limitPrice = Utilities.getLimitPriceForOrder(Parameters.symbol, symbolid, referenceid, order.getOrderSide(), getTickSize(), order.getOrderType());
-                    if(order.getOrderType().equals(EnumOrderType.CUSTOMREL)){
+                    if (order.getOrderType().equals(EnumOrderType.CUSTOMREL)) {
                         order.setLimitPrice(limitPrice);
                     }
                     order.setOrderStage(EnumOrderStage.INIT);
@@ -402,31 +396,31 @@ public class Manual extends Strategy implements TradeListener {
         Type type = new TypeToken<OrderBean>() {
         }.getType();
         Gson gson = new GsonBuilder().create();
-        
+
         OrderBean ob = gson.fromJson((String) line, type);
         //fill default values if missing
-        
+
         int symbolid = Utilities.getIDFromDisplayName(Parameters.symbol, ob.getParentDisplayName());
         if (symbolid == -1 && (ob.getOrderSide().equals(EnumOrderSide.BUY) || ob.getOrderSide().equals(EnumOrderSide.SHORT))) {
             //new symbol. insert symbol into database
             insertSymbol(Parameters.symbol, ob.getParentDisplayName(), optionPricingUsingFutures);
             symbolid = Utilities.getIDFromDisplayName(Parameters.symbol, ob.getParentDisplayName());
         }
-        if(symbolid==-1){
-            logger.log(Level.SEVERE,"Unable to process order {0} as symbol could not be added to instrat",new Object[]{line});
+        if (symbolid == -1) {
+            logger.log(Level.SEVERE, "Unable to process order {0} as symbol could not be added to instrat", new Object[]{line});
             return null;
         }
         int referenceid = getUnderlyingReferenceID(symbolid);
-        if(ob.getLimitPrice()==0){
-        double limitPrice = Utilities.getLimitPriceForOrder(Parameters.symbol, symbolid, referenceid, ob.getOrderSide(), getTickSize(), ob.getOrderType());
-        if(ob.getOrderType().equals(EnumOrderType.CUSTOMREL)){
-            ob.setLimitPrice(limitPrice);
+        if (ob.getLimitPrice() == 0) {
+            double limitPrice = Utilities.getLimitPriceForOrder(Parameters.symbol, symbolid, referenceid, ob.getOrderSide(), getTickSize(), ob.getOrderType());
+            if (ob.getOrderType().equals(EnumOrderType.CUSTOMREL)) {
+                ob.setLimitPrice(limitPrice);
+            }
         }
-        }
-        if(ob.getOriginalOrderSize()==0){
+        if (ob.getOriginalOrderSize() == 0) {
             ob.setOriginalOrderSize(ob.getStrategyOrderSize());
         }
-        if(ob.getStartingPosition()==0){
+        if (ob.getStartingPosition() == 0) {
             ob.setStartingPosition(ob.getStrategyStartingPosition());
         }
         ob.setStartingPosition(Utilities.getNetPosition(Parameters.symbol, getPosition(), symbolid, true));
@@ -455,24 +449,23 @@ public class Manual extends Strategy implements TradeListener {
         }
         ob.setOriginalOrderSize(size);
 
-
         return ob;
     }
 
     public int placeOrder(OrderBean order) {
         int orderid = -1;
         if (order != null) {
-            if(order.getOriginalOrderSize()>0){
-            if ((order.getOrderType() != EnumOrderType.MKT && order.getLimitPrice() > 0) || order.getOrderType().equals(EnumOrderType.MKT)) {
-                logger.log(Level.INFO, "501,Strategy {0},{1}", new Object[]{order.getOrderSide().toString(), getStrategy() + delimiter + order.getOrderSide().toString() + delimiter + order.getParentDisplayName()});
-                if (order.getOrderSide().equals(EnumOrderSide.BUY) || order.getOrderSide().equals(EnumOrderSide.SHORT)) {
-                    orderid = entry(order);
-                } else {
-                    orderid = exit(order);
+            if (order.getOriginalOrderSize() > 0) {
+                if ((order.getOrderType() != EnumOrderType.MKT && order.getLimitPrice() > 0) || order.getOrderType().equals(EnumOrderType.MKT)) {
+                    logger.log(Level.INFO, "501,Strategy {0},{1}", new Object[]{order.getOrderSide().toString(), getStrategy() + delimiter + order.getOrderSide().toString() + delimiter + order.getParentDisplayName()});
+                    if (order.getOrderSide().equals(EnumOrderSide.BUY) || order.getOrderSide().equals(EnumOrderSide.SHORT)) {
+                        orderid = entry(order);
+                    } else {
+                        orderid = exit(order);
+                    }
                 }
-            }
-            }else{
-                logger.log(Level.INFO, "101,Order not placed as ordersize<=0, OrderSize:{0}",new Object[]{order.getOriginalOrderSize()});
+            } else {
+                logger.log(Level.INFO, "101,Order not placed as ordersize<=0, OrderSize:{0}", new Object[]{order.getOriginalOrderSize()});
             }
         } else {
             logger.log(Level.SEVERE, "101,Null Order Object Received");
@@ -537,8 +530,8 @@ public class Manual extends Strategy implements TradeListener {
         dir = Paths.get(p.getProperty("dir", "").trim()).toAbsolutePath();
         orderSource = p.getProperty("OrderSource", "redis").trim();
         optionPricingUsingFutures = Boolean.valueOf(p.getProperty("OptionPricingUsingFutures", "TRUE"));
-        useCashReferenceForSLTP=Boolean.valueOf(p.getProperty("UseCashForSLTP","FALSE"));
-        String entryScanTime = p.getProperty("ScanStartTime","").trim();
+        useCashReferenceForSLTP = Boolean.valueOf(p.getProperty("UseCashForSLTP", "FALSE"));
+        String entryScanTime = p.getProperty("ScanStartTime", "").trim();
         Calendar calToday = Calendar.getInstance(TimeZone.getTimeZone(Algorithm.timeZone));
         String[] entryTimeComponents = entryScanTime.split(":");
         if (entryTimeComponents.length == 3) {
